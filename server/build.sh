44090904378f31c03213c6770e6ff0290e80fbfb
Erik Erlandson <eerlands@redhat.com>
2017-03-16 14:08:46 -0700

    experimental server/consumer in python

---
+++ server/build.sh
@@ -0,0 +1,2 @@
+docker build -t manyangled/metric-digest-server:latest .
+docker push manyangled/metric-digest-server:latest
---
+++ server/server.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import threading, logging, time
+
+from kafka import KafkaConsumer, KafkaProducer
+
+from flask import Flask, request, jsonify, render_template
+
+broker = 'apache-kafka:9092'
+topic = 'my-topic'
+
+latest = None
+
+class Producer(threading.Thread):
+    daemon = True
+
+    def run(self):
+        producer = KafkaProducer(bootstrap_servers=broker)
+        n = 1
+        while True:
+            v = b"ponies!! %d" % (n)
+            producer.send(topic, v)
+            n = n + 1
+            time.sleep(2)
+
+class Consumer(threading.Thread):
+    daemon = True
+
+    def run(self):
+        global latest
+
+        consumer = KafkaConsumer(bootstrap_servers=broker,
+                                 auto_offset_reset='latest')
+        consumer.subscribe([topic])
+
+        for message in consumer:
+            latest = message.value
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def flask_root():
+    return jsonify({"message": "the root of all evil"})
+
+@app.route("/data")
+def flask_data():
+    return jsonify({"message": latest})
+
+threads = [
+    Producer(),
+    Consumer()
+]
+
+for t in threads:
+    t.start()
+
+app.run(host='0.0.0.0', port=8080)
+
+time.sleep(300)
